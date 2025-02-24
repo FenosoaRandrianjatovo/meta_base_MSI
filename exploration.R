3f255d9c4fe886cef87af697a60ec4b40b3020bf
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-02-24 17:33:09 -0500

    Create exploration.R

---
+++ exploration.R
@@ -0,0 +1,14 @@
+# Source of the code: https://www.bioconductor.org/packages/release/bioc/vignettes/Cardinal/inst/doc/Cardinal3-guide.html
+library(Cardinal)
+
+setwd("/home/fenosoa/scratch/Maya_Project/meta_base_MSI_data")
+
+path_nm <- "nm.imzML"
+path_pm <-  "pm.imzML"
+msa_nm <- readMSIData(path_nm)
+msa_pm <- readMSIData(path_pm)
+#  dim(msa_nm)==dim(msa_pm)
+# [1] 20000 13961
+
+spectraData(msa_nm)
+
