1059973b9fee28920b73541db841f7f08f86ab56
FenosoaRandrianjatovo <frandrianjatovo@aimsammi.org>
2025-02-25 11:00:55 -0500

    Live for image

--- exploration.R
+++ exploration.R
@@ -12,18 +12,34 @@ library(grid)
 setwd("/Users/fenosoa/Downloads/Maya_Paper_2025/MetaBase_Data/")
 
 
-
+# Load the data
 path_nm <- "nm.imzML"
 path_pm <-  "pm.imzML"
 msa_nm <- readMSIData(path_nm)
 msa_pm <- readMSIData(path_pm)
 
 
-spectraData(msa_nm)
+# Normalize intensities
+msa_nm <- normalize(msa_nm, method = "tic")
+# Perform baseline correction
+msa_nm <- reduceBaseline(msa_nm)
+# Smooth the spectra
+msa_nm <- smoothSignal(msa_nm, method = "gaussian", sigma = 2)
+# Peak picking
+msa_nm <- peakPick(msa_nm, SNR = 6)
+# Process the queued steps
+msa_nm <- process(msa_nm)
 
-p <- image(msa_pm, i=11, j=11, zlim=c(0, 1e5), main="Positive Mode")
-p
-png(file="msa_pm.png", width=480, height=480)
-dev.off()
+# Normalize intensities
+msa_pm <- normalize(msa_pm, method = "tic")
+# Perform baseline correction
+msa_pm <- reduceBaseline(msa_pm)
+# Smooth the spectra
+msa_pm <- smoothSignal(msa_pm, method = "gaussian", sigma = 2)
+# Peak picking
+msa_pm <- peakPick(msa_pm, SNR = 6)
+# Process the queued steps
+msa_pm <- process(msa_pm)
+
 
 
