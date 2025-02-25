778ac9ed5e972cb097a2b3e4f733c05647be632f
FenosoaRandrianjatovo <frandrianjatovo@aimsammi.org>
2025-02-25 08:23:37 -0500

    Live for image

--- exploration.R
+++ exploration.R
@@ -1,5 +1,9 @@
 # Source of the code: https://www.bioconductor.org/packages/release/bioc/vignettes/Cardinal/inst/doc/Cardinal3-guide.html
 library(Cardinal)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
 
 # Online Working directory
 # setwd("/home/fenosoa/scratch/Maya_Project/meta_base_MSI_data")
@@ -13,11 +17,10 @@ path_nm <- "nm.imzML"
 path_pm <-  "pm.imzML"
 msa_nm <- readMSIData(path_nm)
 msa_pm <- readMSIData(path_pm)
-#  dim(msa_nm)==dim(msa_pm)
-# [1] 20000 13961
+d
 
 spectraData(msa_nm)
-p <- image(msa_pm, i=1978)
+p <- image(msa_pm, i=11, j=11, zlim=c(0, 1e5), main="Positive Mode")
 p
 ggsave("Image.png", plot = p , width = 20, height = 15, dpi = 300)
 
