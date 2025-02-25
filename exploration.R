e671346dbf9baeae9a2052f88910d7fd18841df9
FenosoaRandrianjatovo <frandrianjatovo@aimsammi.org>
2025-02-25 08:06:37 -0500

    Local wd

--- exploration.R
+++ exploration.R
@@ -1,7 +1,12 @@
 # Source of the code: https://www.bioconductor.org/packages/release/bioc/vignettes/Cardinal/inst/doc/Cardinal3-guide.html
 library(Cardinal)
+# Online Working directory
+# setwd("/home/fenosoa/scratch/Maya_Project/meta_base_MSI_data")
 
-setwd("/home/fenosoa/scratch/Maya_Project/meta_base_MSI_data")
+# Local wd
+setwd("/Users/fenosoa/Downloads/Maya_Paper_2025/MetaBase_Data/meta_base_MSI")
+
+
 
 path_nm <- "nm.imzML"
 path_pm <-  "pm.imzML"
@@ -11,7 +16,9 @@ msa_pm <- readMSIData(path_pm)
 # [1] 20000 13961
 
 spectraData(msa_nm)
+p <- image(msa_pm, i=1978)
+ggsave("Image.png", plot = p , width = 20, height = 15, dpi = 300)
 
-ggsave("Image.png", plot = image(msa_pm, i=1978) , width = 20, height = 15, dpi = 300)
 
 
+e
