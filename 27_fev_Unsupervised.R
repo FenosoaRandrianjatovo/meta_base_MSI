c532fd9658011b3e20f268aa3f166d33a0c5d32e
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-03-02 16:00:33 -0500

    Update 27_fev_Unsupervised.R

--- 27_fev_Unsupervised.R
+++ 27_fev_Unsupervised.R
@@ -87,7 +87,7 @@ msa_pm <- summarizePixels(msa_pm, c(TIC="sum"))
 png(filename = "images/summarizePixels_pm.png", width = 1000, height = 800, res = 100)
 image(msa_pm, "TIC")
 dev.off()
-
+# Do this only in Compute Canada, otherwise if have good resource
 msa_pm_peaks <- msa_pm |>
   normalize(method="tic") |>
   peakProcess(SNR=3, sampleSize=0.1,
