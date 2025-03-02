ba3538fb69d8fae3d71902c58a90f6ee7203a926
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-03-02 18:55:17 -0500

    Update Statistical_testing_for_MSI.R

--- Statistical_testing_for_MSI.R
+++ Statistical_testing_for_MSI.R
@@ -19,3 +19,34 @@ msa_pm_peaks <- msa_pm |>
     normalize(method="tic") |>
     peakProcess(SNR=3, sampleSize=0.1, filterFreq=0.2,
         tolerance=0.5, units="mz")
+
+# 2.1.1Non-specific filtering to reduce data size
+# In order to reduce the size of the dataset further (because the computation we are working toward can be time consuming), we will perform non-specific filtering.
+
+# This means filtering our peaks based on a summary statistic unrelated to the condition. We will use the variance.
+msa_pm_peaks <- summarizeFeatures(msa_pm_peaks, stat=c(Variance="var"))
+
+# Now we keep only the peaks above the top 80% quantile of variance among peaks.
+msa_pm_peaks_80 <- subsetFeatures(msa_pm_peaks, Variance >= quantile(Variance, 0.8))
+
+# This reduces the dataset msa_pm_peaks_80  to only 26 peaks.
+
+# Segmentation with spatial Dirichlet Gaussian mixture model (DGMM)
+
+set.seed(1)
+msa_pm_peaks_80dgmm <- spatialDGMM(msa_pm_peaks_80, r=1, k=3, groups=msa_pm_peaks_80$run)
+
+msa_pm_peaks_80dgmm
+
+
+
+
+
+
+
+
+
+
+
+
+
