dcdbeccd3a8e984114f6d638d71d9128863b56c2
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-03-02 18:49:18 -0500

    Update Statistical_testing_for_MSI.R

--- Statistical_testing_for_MSI.R
+++ Statistical_testing_for_MSI.R
@@ -9,3 +9,13 @@ path_nm <- "nm.imzML"
 path_pm <-  "pm.imzML"
 msa_nm <- readMSIData(path_nm)
 msa_pm <- readMSIData(path_pm)
+
+# Pre-processing for msa_pm
+msa_pm <- summarizePixels(msa_pm, stat=c(TIC="sum"))
+
+# We will normalize the TIC, perform peak picking on a sample of 10% of all mass spectra to create a set of reference peaks, 
+# and then summarize these reference peaks in every spectrum.
+msa_pm_peaks <- msa_pm |>
+    normalize(method="tic") |>
+    peakProcess(SNR=3, sampleSize=0.1, filterFreq=0.2,
+        tolerance=0.5, units="mz")
